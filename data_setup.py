87667052200585fb3b8bd455b5bd2a5c8a33781c
remi-rosenthal <remi.rosenthal@eleves.ec-nantes.fr>
2018-05-24 16:30:07 +0100

    added silence generator + files

--- data_setup.py
+++ data_setup.py
@@ -1,5 +1,9 @@
 from os import listdir
 from os.path import isfile, join
+import scipy.io.wavfile as scwav
+import numpy as np
+import acoustics
+from random import randint, uniform
 
 def make_training_list():
     onlyfiles = listdir('../Data/train/audio')
@@ -22,6 +26,42 @@ def make_training_list():
     for filename in allfiles:
         if filename not in notTrain:
             train_file.write(filename+'\n')
+
+
+def silence_generator():
+    path = '../Data/train/audio/_background_noise_'
+    nsamples = 1700
+    noise_list = [f for f in listdir(path) if isfile(join(path, f))]
+    noise_list.remove('README.md')
+    
+    for i in range(nsamples):
+        #select random noise effect
+        selected = noise_list[randint(0, len(noise_list)-1)]
+        _, sample = scwav.read('../Data/train/audio/_background_noise_/'+selected)
+        #select random start index over 60s
+        start_index = randint(0, len(sample)-16000)
+        #copy 1s after start index
+        new_sample = sample[start_index:start_index+16000]
+        #randomly lower the intensity
+        decrease = uniform(0, 0.6)
+        new_sample = decrease*new_sample
+        new_sample = np.rint(new_sample).astype('int16')
+        #write file
+        scwav.write('../Data/train/audio/silence/silent'+str(i), 16000, new_sample)
+
+#silence_generator()
+
+def append_silence(filename):
+    with open(filename, 'a') as myfile:
+        noise_list = [f for f in listdir('../Data/train/audio/silence') if isfile(join('../Data/train/audio/silence', f))]
+        for i in range(1400):
+            myfile.write('silence/'+noise_list[i]+'.wav'+'\n')
+
+#append_silence('../Data/train/training_list.txt')
+
+
+
+   
 
 
 
