fix gradient direction

model_2/dataset_mfcc.py
@@ -145,9 +145,9 @@ class SRCdataset(Dataset):
 
         # compute mfccs & first & second order gradient
         mfccs = mfcc(new_sample, seq_length, n_mfcc=23, n_fft=640, hop_length=320) #n_mfcc x 51
-        grad_mfccs = np.gradient(mfccs, axis = 0)
+        grad_mfccs = np.gradient(mfccs, axis = 1)
         mfccs = np.concatenate((mfccs, grad_mfccs)) #2*n_mfcc x 51
-        mfccs = np.concatenate((mfccs, np.gradient(grad_mfccs, axis = 0))) #3*n_mfcc x 51
+        mfccs = np.concatenate((mfccs, np.gradient(grad_mfccs, axis = 1))) #3*n_mfcc x 51
         mfccs = torch.from_numpy(mfccs)
         mfccs = mfccs.type(torch.FloatTensor)
 

model_2/model_mfcc.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 
 class Network(nn.Module):
 
-    def __init__(self, num_features = 512, num_layers = 2):
+    def __init__(self, num_features = 69, num_layers = 2):
         super(Network, self).__init__()
         self.gru = nn.GRU(69, num_features, num_layers = num_layers, bidirectional = True, batch_first = True)
         self.fc = nn.Linear(num_features*2, 12)